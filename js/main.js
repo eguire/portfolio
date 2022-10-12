bcdd569ce199feccfe9d145d5d31f905eae0dce0
eguire <105729902+eguire@users.noreply.github.com>
2022-10-12 19:19:08 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,3 +8,27 @@ $(function(){
 		$('.menu__list').toggleClass('menu__list--active');
 		$('body').toggleClass('scroll-lock');
 	});
+
+	$(window).scroll(function(){
+		if ($(window).scrollTop() >=menuHeight) {
+			$('.menu').addClass('sticky')
+		} else {
+			$('.menu').removeClass('sticky')
+		}
+	});
+
+	const headerHeight = $('.header').outerHeight();
+
+	$('.menu__link, .logo, .header__arrow').on('click', function(e){
+		e.preventDefault();
+
+		const scrollAnchor = $(this).attr('href');
+
+		let scrollPoint = $(scrollAnchor).offset().top - menuHeight;
+
+		$('html, body').animate({
+			scrollTop: scrollPoint
+		}, 1000);
+	});
+
+	const menuHeight = $('.menu').outerHeight();
